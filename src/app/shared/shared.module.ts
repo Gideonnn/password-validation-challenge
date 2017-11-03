e9de7ed6547d8521295280b951ef6dce22e4a929
Gideon Spierings <gspierings@vaa.com>
2017-11-03 18:32:24 +0100

    Added the Google Material Design component lib

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,20 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Modules
+import { MaterialModule } from './modules/material.module';
+
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
   ],
   declarations: [
 
   ],
   exports: [
     CommonModule,
+    MaterialModule,
   ],
 })
 export class SharedModule { }
